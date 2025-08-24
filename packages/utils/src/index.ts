c1c8fb3bc38423f288922cc4d18505731e8cf7d1
Neilfb <49308761+Neilfb@users.noreply.github.com>
2025-08-24 22:14:51 +0100

    Fix: Update utils index.ts to only export existing files

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,3 @@
 export * from "./helpers";
-export * from './validation';
-export * from './allergens';
-export * from './storage/uploadService';
-export * from './qr/qrGenerator';
 export * from './performance/cacheService';
 export * from './seo/seoUtils';
